070fefd65fed9793bc11553012cc60f9cec18c85
abdullahqayid <3boo0odq0v@gmail.com>
2024-06-28 00:14:36 +0300

    installing shadcn-ui & setting up proper folder structure done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 export default function HomePage() {
-  return <h3 className="text-3xl">Home page</h3>;
+  return (
+    <div>
+      <h3 className="text-3xl">Home page</h3>
+      <Button variant="outline" size="lg" className="capitalize m-8">
+        Button works!
+      </Button>
+    </div>
+  );
 }
