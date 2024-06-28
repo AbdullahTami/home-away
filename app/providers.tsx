41d5f55e8437b6e893281703f57521845afe12f4
abdullahqayid <3boo0odq0v@gmail.com>
2024-06-28 17:44:06 +0300

    setting up clerk auth & middleware file done

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,10 +2,12 @@
 
 import React from "react";
 import { ThemeProvider } from "./ThemeProvider";
+import { Toast } from "@/components/ui/toast";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Toast />
       <ThemeProvider
         attribute="class"
         defaultTheme="system"
