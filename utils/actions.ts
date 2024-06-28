b3d2e2a2dd49d1f5c2c042bba25664fb39678047
abdullahqayid <3boo0odq0v@gmail.com>
2024-06-28 22:52:19 +0300

    successfully setting up update user profile functionality done

--- utils/actions.ts
+++ utils/actions.ts
@@ -4,6 +4,23 @@ import { clerkClient, currentUser } from "@clerk/nextjs/server";
 import { profileSchema } from "./schemas";
 import prisma from "./db";
 import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
+import { error } from "console";
+
+async function getAuthUser() {
+  const user = await currentUser();
+  if (!user) throw new Error("You must be logged in to access this route");
+  if (!user.privateMetadata.hasProfile) redirect("/profile/create");
+  return user;
+}
+
+function renderError(error: unknown): { message: string } {
+  console.log(error);
+
+  return {
+    message: error instanceof Error ? error.message : "An error occurred",
+  };
+}
 
 export async function createProfileAction(prevState: any, formData: FormData) {
   try {
@@ -28,9 +45,7 @@ export async function createProfileAction(prevState: any, formData: FormData) {
     });
   } catch (error) {
     console.log(error);
-    return {
-      message: error instanceof Error ? error.message : "An error occurred",
-    };
+    return renderError(error);
   }
   redirect("/");
 }
@@ -48,4 +63,46 @@ export async function fetchProfileImage() {
     },
   });
   return profile?.profileImage;
+}
+
+export async function fetchProfile() {
+  const user = await getAuthUser();
+  const profile = await prisma.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+  });
+  if (!profile) redirect("/profile/create");
+
+  return profile;
+}
+
+export async function updateProfileAction(
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> {
+  const user = await getAuthUser();
+
+  try {
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = profileSchema.safeParse(rawData);
+    console.log(validatedFields);
+
+    if (!validatedFields.success) {
+      const errors = validatedFields.error.issues.map((error) => error.message);
+      throw new Error(errors.join(","));
+    }
+
+    await prisma.profile.update({
+      where: {
+        clerkId: user.id,
+      },
+      data: validatedFields.data,
+    });
+
+    revalidatePath("/profile");
+    return { message: "Profile updated successfully" };
+  } catch (error) {
+    return renderError(error);
+  }
 }
