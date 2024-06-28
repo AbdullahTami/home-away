5c079b2f7000949fcd4520d6b769c4de32135bf9
abdullahqayid <3boo0odq0v@gmail.com>
2024-06-28 22:02:45 +0300

    successfully setting up db with prisma ORM & fetching user credentials done

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,6 +1,16 @@
+import { fetchProfileImage } from "@/utils/actions";
 import React from "react";
 import { LuUser2 } from "react-icons/lu";
 
-export default function UserIcon() {
-  return <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white" />;
+export default async function UserIcon() {
+  const profileImage = await fetchProfileImage();
+  return profileImage ? (
+    <img
+      src={profileImage}
+      alt="profile image"
+      className="w-6 h-6 rounded-full object-cover"
+    />
+  ) : (
+    <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white" />
+  );
 }
--- utils/actions.ts
+++ utils/actions.ts
@@ -1,15 +1,51 @@
 "use server";
 
+import { clerkClient, currentUser } from "@clerk/nextjs/server";
 import { profileSchema } from "./schemas";
+import prisma from "./db";
+import { redirect } from "next/navigation";
 
 export async function createProfileAction(prevState: any, formData: FormData) {
   try {
+    const user = await currentUser();
+    if (!user) throw new Error("Please login to create a profile");
+    console.log(user);
+
     const rawData = Object.fromEntries(formData);
     const validatedFields = profileSchema.parse(rawData);
-    console.log(validatedFields);
-    return { message: "Profile created..." };
+    await prisma.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? "",
+        ...validatedFields,
+      },
+    });
+    await clerkClient.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        hasProfile: true,
+      },
+    });
   } catch (error) {
     console.log(error);
-    return { message: "There was an error..." };
+    return {
+      message: error instanceof Error ? error.message : "An error occurred",
+    };
   }
+  redirect("/");
+}
+
+export async function fetchProfileImage() {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const profile = await prisma.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+    select: {
+      profileImage: true,
+    },
+  });
+  return profile?.profileImage;
 }
