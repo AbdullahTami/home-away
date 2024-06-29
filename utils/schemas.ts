180ba895cf9480db91f33825fe7269862bfdc89c
abdullahqayid <3boo0odq0v@gmail.com>
2024-06-29 17:42:52 +0300

    setting up property model & schema successfully done

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -1,5 +1,6 @@
 import * as z from "zod";
 
+//! Profile schema
 export const profileSchema = z.object({
   firstName: z.string().min(2, {
     message: "first name must be at least 2 characters",
@@ -12,6 +13,7 @@ export const profileSchema = z.object({
   }),
 });
 
+//!Helper function
 export function validateWithZodSchema<T>(
   schema: z.ZodSchema<T>,
   data: unknown
@@ -25,6 +27,7 @@ export function validateWithZodSchema<T>(
   return result.data;
 }
 
+//! Image upload schema
 export const imageSchema = z.object({
   image: validateFile(),
 });
@@ -43,3 +46,49 @@ function validateFile() {
       );
     }, "File must be an image");
 }
+//! Properties schema
+export const propertySchema = z.object({
+  name: z
+    .string()
+    .min(2, {
+      message: "name must be at least 2 characters.",
+    })
+    .max(100, {
+      message: "name must be less than 100 characters.",
+    }),
+  tagline: z
+    .string()
+    .min(2, {
+      message: "tagline must be at least 2 characters.",
+    })
+    .max(100, {
+      message: "tagline must be less than 100 characters.",
+    }),
+  price: z.coerce.number().int().min(0, {
+    message: "price must be a positive number.",
+  }),
+  category: z.string(),
+  description: z.string().refine(
+    (description) => {
+      const wordCount = description.split(" ").length;
+      return wordCount >= 10 && wordCount <= 1000;
+    },
+    {
+      message: "description must be between 10 and 1000 words.",
+    }
+  ),
+  country: z.string(),
+  guests: z.coerce.number().int().min(0, {
+    message: "guest amount must be a positive number.",
+  }),
+  bedrooms: z.coerce.number().int().min(0, {
+    message: "bedrooms amount must be a positive number.",
+  }),
+  beds: z.coerce.number().int().min(0, {
+    message: "beds amount must be a positive number.",
+  }),
+  baths: z.coerce.number().int().min(0, {
+    message: "bahts amount must be a positive number.",
+  }),
+  amenities: z.string(),
+});
