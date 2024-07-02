7303f7872d6f3c74f4335bf432c5ef8463ebc8bf
abdullahqayid <3boo0odq0v@gmail.com>
2024-07-02 17:57:02 +0300

    authorizing fav toggle nutton to logged in users only done

--- components/card/FavoriteToggleButton.tsx
+++ components/card/FavoriteToggleButton.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Button } from "../ui/button";
 import { FaHeart } from "react-icons/fa";
+import { auth } from "@clerk/nextjs/server";
+import { CardSignInButton } from "../form/Buttons";
 
-export default function FavoriteToggleButton({
+export default async function FavoriteToggleButton({
   propertyId,
 }: {
   propertyId: string;
 }) {
+  const { userId } = auth();
+  if (!userId) return <CardSignInButton />;
   return (
     <Button size="icon" variant="outline" className="p-2 cursor-pointer">
       <FaHeart />
--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -3,6 +3,8 @@
 import { useFormStatus } from "react-dom";
 import { Button } from "../ui/button";
 import { ReloadIcon } from "@radix-ui/react-icons";
+import { SignInButton } from "@clerk/nextjs";
+import { FaRegHeart } from "react-icons/fa";
 
 type btnSize = "default" | "lg" | "sm";
 
@@ -35,5 +37,21 @@ export function SubmitButton({
         text
       )}
     </Button>
+  );
+}
+
+export function CardSignInButton() {
+  return (
+    <SignInButton mode="modal">
+      <Button
+        type="button"
+        size="icon"
+        variant="outline"
+        className="p-2 cursor-pointer"
+        asChild
+      >
+        <FaRegHeart />
+      </Button>
+    </SignInButton>
   );
 }
