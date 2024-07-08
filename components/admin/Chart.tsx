aa36d708b890ccb0a0312af580c470e9f7e3a6ab
abdullahqayid <3boo0odq0v@gmail.com>
2024-07-08 22:45:50 +0300

    charts display & admin page done

--- components/admin/Chart.tsx
+++ components/admin/Chart.tsx
@@ -1,5 +1,34 @@
+"use client";
 import React from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
-export default function Chart() {
-  return <div>Chart</div>;
+type ChartPropsType = {
+  data: {
+    date: string;
+    count: number;
+  }[];
+};
+export default function Chart({ data }: ChartPropsType) {
+  return (
+    <section className="mt-28">
+      <h1 className="text-4xl font-mono text-center">Monthly Bookings</h1>
+      <ResponsiveContainer width="100%" height={300}>
+        <BarChart data={data} margin={{ top: 50 }}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" />
+          <YAxis allowDecimals={false} />
+          <Tooltip />
+          <Bar dataKey="count" fill="#f97215" barSize={75} />
+        </BarChart>
+      </ResponsiveContainer>
+    </section>
+  );
 }
